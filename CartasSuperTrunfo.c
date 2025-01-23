15be7b1b0b39dc4eb3b20bf802e900f47f96459a
ErickAucaZ <aucarioafg123@gmail.com>
2025-01-23 19:22:53 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,43 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    /*Atributos da cidade*/
+
+    int codigodacidade;
+    char nome[30];
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
+
+    /*Utilização da funções printf e scanf para o úsuario definir cada atributo da cidade*/
+
+    printf("Código da cidade: /n");
+    scanf("%d", &codigodacidade);
+
+    printf("Nome da cidade: \n");
+    scanf("%s", &nome);
+
+    printf("População da cidade: \n");
+    scanf("%d", &populacao);
+
+    printf("Área da cidade: \n");
+    scanf("%f", &area);
+
+    printf("PIB da cidade: \n");
+    scanf("%f", &pib);
+
+    printf("Número de pontos turísticos: \n");
+    scanf("%d", &pontosturisticos);
+
+    /*Resumo de todos atributos que o úsuario deu a cidade*/
+
+    printf("Código da cidade: %d\n", codigodacidade);
+    printf("Nome da cidade: %s\n", nome);
+    printf("População da cidade: %d\n", populacao);
+    printf("Área da cidade: %f\n", area);
+    printf("PIB da cidade: %f\n", pib);
+    printf("Números de pontos turísticos da cidade: %d\n", pontosturisticos);
+
     return 0;
 }
